e9f770551c9e754380b48bfcb9a9bf3ea472db7c
mhieu <mhieudzvcl@gmail.com>
2025-12-17 00:04:06 +0700

    add follow activity UI and add followersfragment, followingfragment layout into tab host of follow activity UI

---
+++ app/src/main/java/com/example/fitup/FollowActivity.java
@@ -0,0 +1,50 @@
+package com.example.fitup;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class FollowActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_follow);
+
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        ImageView btnBack = findViewById(R.id.imgbtn_Back);
+        TextView tvTitle = findViewById(R.id.tvTitle);
+
+        tvTitle.setText("Follow");
+        btnBack.setOnClickListener(v -> finish());
+
+        FollowViewPagerAdapter adapter = new FollowViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            if (position == 0) {
+                tab.setText("3 Followers");
+            } else {
+                tab.setText("3 Following");
+            }
+        }).attach();
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
